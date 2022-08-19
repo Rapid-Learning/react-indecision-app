fc11fdad30e06358a7f578c152bfed9f313f6ca0
jayparmar271 <jayparmar271@gmail.com>
2022-08-19 22:11:10 +0530

    Add: Form to add option

--- src/app.js
+++ src/app.js
@@ -27,26 +27,35 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handle pick')
+    }
+
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
-  render() {
-    return (
-        <div>
-            {
-                this.props.options.map((option) => <Option key={option} optionText={option} />)
-            }
-            <Option />
-        </div>
-    );
-  }
+    handleRemoveAll() {
+        alert('Remove all');
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
+                <Option />
+            </div>
+        );
+    }
 }
 
 class Option extends React.Component {
@@ -60,10 +69,23 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+
+        if (option) {
+            alert(option)
+        }
+    }
+
     render() {
         return (
             <div>
-                Add option component here
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" />
+                    <button>Add option</button>
+                </form>
             </div>
         );
     }
