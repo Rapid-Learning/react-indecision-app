5d87ab009beed61a5c3a6e3c9bbcc4354bc75e03
jayparmar271 <jayparmar271@gmail.com>
2022-08-22 00:11:55 +0530

    Add: Modal to show selected option

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Options from './Options';
 import Header from './Header';
 import Action from './Action';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
 
     componentDidMount() {
@@ -33,6 +35,10 @@ export default class IndecisionApp extends React.Component {
         this.setState(() => ({ options: [] }));
     }
 
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }))
+    }
+
     handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({ 
             options: prevState.options.filter((option) => {
@@ -44,7 +50,7 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({selectedOption: option})) 
     }
 
     handleAddOption = (option) => {
@@ -75,6 +81,10 @@ export default class IndecisionApp extends React.Component {
                     handleDeleteOption={this.handleDeleteOption}
                 />
                 <AddOption handleAddOption={this.handleAddOption} />
+                <OptionModal 
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
+                />
             </div>
         );
     }
